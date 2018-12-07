eafa0d5648ad2a3e0fd468fdc111393b85bd8e62
Simon Repp <simon@fdpl.io>
2018-12-07 16:20:14 +0100

    Add community projects, link github in footer, blog update

--- src/footer.js
+++ src/footer.js
@@ -1,7 +1,15 @@
 module.exports = () => `
   <footer class="footer">
     <div class="boundary">
-      2018 - The eno notation language and libraries. &nbsp; <a href="/">Home</a> &nbsp; <a href="/about/">About</a> &nbsp; <a href="http://eepurl.com/dA9LcH" target="_blank">Newsletter</a>  
+      2018 - The eno notation language and libraries.
+      &nbsp;&nbsp;
+      <a href="/">Home</a>
+      &nbsp;&nbsp;
+      <a href="/about/">About</a>
+      &nbsp;&nbsp;
+      <a href="https://github.com/eno-lang/" target="_blank">GitHub</a>
+      &nbsp;&nbsp;
+      <a href="http://eepurl.com/dA9LcH" target="_blank">Newsletter</a>
     </div>
   </footer>
 `;
