d8eb3dcbe018f79b2c3bdd1e9604b33eeca00a59
Simon Repp <simon@fdpl.io>
2020-05-23 09:16:39 +0200

    Remove turblinks, flatten footer code and use semantic element

--- src/layout.js
+++ src/layout.js
@@ -1,35 +1,40 @@
-const footer = require('./footer.js');
 const header = require('./header.js');
 
 // TODO: Generic github contribution/edit link on every page through the layout and a meta field in all pages
 
-module.exports = (data, content, title, breadcrumb, activeUrl = null) => {
-  const html = `
-    <!doctype html>
-    <html>
+module.exports = (data, content, title, breadcrumb, activeUrl = null) => `
+  <!doctype html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="description" content="The eno notation language and libraries">
 
-      <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="The eno notation language and libraries">
+      <title>${title}</title>
 
-        <title>${title}</title>
+      <link rel="stylesheet" href="/styles.css">
+    </head>
 
-        <link rel="stylesheet" href="/styles.css">
-        <script src="/scripts.js"></script>
-      </head>
+    <body>
+      ${header(data, breadcrumb, activeUrl)}
 
-      <body>
-        ${header(data, breadcrumb, activeUrl)}
+      <div class="boundary header_offset padding">
+        ${content}
+      </div>
 
-        <div class="boundary header_offset padding">
-          ${content}
+      <footer>
+        <div class="boundary">
+          2020 - The eno notation language and libraries.
+          &nbsp;&nbsp;
+          <a href="/">Home</a>
+          &nbsp;&nbsp;
+          <a href="/about/">About</a>
+          &nbsp;&nbsp;
+          <a href="https://github.com/eno-lang/" target="_blank">GitHub</a>
+          &nbsp;&nbsp;
+          <a href="http://eepurl.com/dA9LcH" target="_blank">Newsletter</a>
         </div>
-
-        ${footer()}
-      </body>
-    </html>
-  `;
-
-  return html;
-};
+      </footer>
+    </body>
+  </html>
+`;
